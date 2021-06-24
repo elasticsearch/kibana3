a5b2709667eade532acec219ce94ca1398972dd1
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-06-24 11:54:36 +0300

    More fixes.

--- src/plugins/presentation_util/public/components/error/error.tsx
+++ src/plugins/presentation_util/public/components/error/error.tsx
@@ -13,16 +13,6 @@ import { i18n } from '@kbn/i18n';
 import { get } from 'lodash';
 import { ShowDebugging } from './show_debugging';
 
-const strings = {
-  getDescription: () =>
-    i18n.translate('xpack.canvas.errorComponent.description', {
-      defaultMessage: 'Expression failed with the message:',
-    }),
-  getTitle: () =>
-    i18n.translate('xpack.canvas.errorComponent.title', {
-      defaultMessage: 'Whoops! Expression failed',
-    }),
-};
 export interface Props {
   payload: {
     error: Error;
