ca4761a7ce9988984c87f36b85b04afff57a299b
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-07-05 16:50:10 +0300

    skeleton of `expression_image` (partial)

---
+++ src/plugins/expression_image/server/plugin.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import {
+  PluginInitializerContext,
+  CoreSetup,
+  CoreStart,
+  Plugin,
+  Logger,
+} from '../../../core/server';
+
+import { ExpressionImagePluginSetup, ExpressionImagePluginStart } from './types';
+import { defineRoutes } from './routes';
+
+export class ExpressionImagePlugin
+  implements Plugin<ExpressionImagePluginSetup, ExpressionImagePluginStart> {
+  private readonly logger: Logger;
+
+  constructor(initializerContext: PluginInitializerContext) {
+    this.logger = initializerContext.logger.get();
+  }
+
+  public setup(core: CoreSetup) {
+    this.logger.debug('expressionImage: Setup');
+    const router = core.http.createRouter();
+
+    // Register server side APIs
+    defineRoutes(router);
+
+    return {};
+  }
+
+  public start(core: CoreStart) {
+    this.logger.debug('expressionImage: Started');
+    return {};
+  }
+
+  public stop() {}
+}
