efaf4d71210524e40211ab28df3fbe4346a5710c
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-07-06 17:00:39 +0300

    Generated `expression_progress`  plugin.

---
+++ src/plugins/expression_progress/public/expression_renderers/__stories__/progress_renderer.stories.tsx
@@ -0,0 +1,28 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Render } from '../../../../presentation_util/public/__stories__';
+import { progressRenderer } from '../progress_renderer';
+import {
+  elasticLogo,
+  elasticOutline,
+} from '../../../../../../src/plugins/presentation_util/common/lib';
+
+storiesOf('renderers/progress', module).add('default', () => {
+  const config = {
+    count: 42,
+    image: elasticLogo,
+    size: 20,
+    max: 60,
+    emptyImage: elasticOutline,
+  };
+
+  return <Render renderer={progressRenderer} config={config} width="400px" />;
+});
